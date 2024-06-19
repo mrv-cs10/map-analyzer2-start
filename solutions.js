0f69739af350ed01aea9c3025010a15b34035932
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2024-06-19 10:45:42 -0600

    Initial commit

---
+++ solutions.js
@@ -0,0 +1,67 @@
+// SOLUTIONS TO MAP ANALYZER LEVELS
+
+// Global Variables
+let row, column; // Row & Column of Mouse on the Map
+let tile = "----"; // Use to store type of tile to display
+
+// Solution Functions...
+function level1Solution() {
+  // USE BINARY IF STATEMENT
+  if (column <= 4) {
+    tile = "Sand";
+  } else {
+    tile = "Water";
+  }
+}
+
+function level2Solution() {
+  // USE CHAINED IF STATEMENT
+}
+
+function level3Solution() {
+  // USE BINARY IF STATEMENT
+}
+
+function level4Solution() {
+  // USE BINARY IF STATEMENT
+}
+
+function level5Solution() {
+  // USE CHAINED IF STATEMENT
+}
+
+function level6Solution() {
+  // USE BINARY IF STATEMENT
+}
+
+function level7Solution() {
+  // USE BINARY IF STATEMENT
+}
+
+function level8Solution() {
+  // USE BINARY IF STATEMENT
+}
+
+function level9Solution() {
+  // USE BINARY IF STATEMENT
+}
+
+function level10Solution() {
+  // USE CHAINED IF STATEMENT
+}
+
+function level11Solution() {
+  // USE CHAINED IF STATEMENT
+}
+
+function level12Solution() {
+  // USE BINARY IF STATEMENT
+}
+
+function level13Solution() {
+  // USE BINARY IF STATEMENT
+}
+
+function level14Solution() {
+  // USE CHAINED IF STATEMENT
+}
